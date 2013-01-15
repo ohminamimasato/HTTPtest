47e4ad024597b6c974eae234289074b407c74dbb
Ohminami.masato <dainan@ntts-sv.co.jp>
2013-01-15 11:51:00 +0900

    readXML(XmlPullParser)を入れる
    readXMLをコールし、入力内容をXmlParserで分析してニュース
    タイトルを抽出するように変更
    タイトルはベタでdataにつながったまま表示されるだけ

--- WebCom1/src/com/dainan/webcom/webcom1.java
+++ WebCom1/src/com/dainan/webcom/webcom1.java
@@ -1,17 +1,28 @@
 package com.dainan.webcom;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Xml;
 import android.widget.TextView;
 
 public class webcom1 extends Activity {
 	private TextView mView;
+
+	// article
+		static private String mArticleTitle[];
+		static private String mArticleURL[];
+		static private int mArticleNum;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,31 +39,63 @@ public class webcom1 extends Activity {
 				category);
 	}
 
-    public static String httpGet(String strURL){
-    	//(1)トライキャッチによるエラー処理
-    	try{
-    		    //(2)URLクラスを使って通信
-    		    URL url=new URL(strURL);
-    		    URLConnection connection=url.openConnection();
-    		   //(3)動作を入力に設定
-    		    connection.setDoInput(true);    //データを入力することの宣言
-    		    InputStream stream=connection.getInputStream();   //(4)入力ストリームを取得
-    		   //(5)得られた入力ストリームをバッファリーダ(input）を使って読み出していく（ための設定）
-    		    BufferedReader input=new BufferedReader(new InputStreamReader(stream));
-    		   //(6)データの取得処理
-    		    String data="";
-    		    String tmp="";
-    		    		while((tmp=input.readLine()) !=null){     //入力ストリームから1行ずつ読み込み、
-    		    			data+=tmp;                                       //dataに次々追加（バッファが空になるまで）
-    		    		}
-    		   //(7)終了処理
-    		     stream.close();
-    		     input.close();
-    		     return data;         //dataを返却
+	public static String httpGet(String strURL) {
+		// (1)try-catchによるエラー処理
+		try {
+			// (2)URLクラスを使って通信
+			URL url = new URL(strURL);
+			URLConnection connection = url.openConnection();
+			// (3)動作を入力に設定
+			connection.setDoInput(true);
+			InputStream stream = connection.getInputStream();
+			readXML(stream);
+			String data="";
+			   for(int i=0;i<mArticleNum;i++){
+				   data+=mArticleTitle[i];
+			   }
+			// (5)終了処理
+			stream.close();
+			return data;
+		} catch (Exception e) {
+			// (6)エラー処理
+			return e.toString();
+		}
+	}
 
-    	}catch (Exception  e){
-    		    //(8)エラー処理
-    		return e.toString();
-    	    }
-    }
+
+
+    public static void readXML(InputStream stream)
+			throws XmlPullParserException {
+		try {
+			XmlPullParser myxmlPullParser = Xml.newPullParser();
+			myxmlPullParser.setInput(stream, "UTF-8");
+
+			int cntTitle = 0;
+			int cntAddress = 0;
+			for (int e = myxmlPullParser.getEventType(); e != XmlPullParser.END_DOCUMENT; e = myxmlPullParser
+					.next()) {
+				if (e == XmlPullParser.START_TAG) {
+					if (myxmlPullParser.getName().equals("ResultSet")) {
+						mArticleNum = Integer
+								.parseInt(myxmlPullParser.getAttributeValue(
+										null, "totalResultsReturned"));
+						mArticleTitle = new String[mArticleNum];
+						mArticleURL = new String[mArticleNum];
+					} else if (myxmlPullParser.getName().equals("Title")) {
+						mArticleTitle[cntTitle] = myxmlPullParser.nextText();
+						cntTitle++;
+					} else if (myxmlPullParser.getName()
+							.equals("SmartphoneUrl")) {
+						mArticleURL[cntAddress] = myxmlPullParser.nextText();
+						cntAddress++;
+					}
+				}
+
+			}
+		} catch (XmlPullParserException e) {
+		} catch (IOException e) {
+		}
+
+	}
+
 }
